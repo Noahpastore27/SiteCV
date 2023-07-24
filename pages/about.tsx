b039fa0e3d1237fa06c0fe54b5ae666cde01bda1
noah.langlois <noahlanglois@hotmail.com>
2023-07-24 21:27:01 +0000

    The first big update

---
+++ pages/about.tsx
@@ -0,0 +1,34 @@
+import Id from "../public/images/Id.png"
+import Image from "next/image";
+export default function About () {
+    return (
+        <>
+                  <div className="row g-0">
+            <div className="col-8">
+              <h1 className="mt-4">LANGLOIS Noah</h1>
+              <p className="mt-3">Actuellement étudiant ingénieur à l&apos;<a href="https://www.ensma.fr/" 
+              target="_blank" rel="noopener noreferrer">ISAE-ENSMA</a> où je me suis spécialisé en informatique
+              et avionique, je suis à la recherche d&apos;un projet de fin d&apos;étude afin d&apos;achever ma formation.</p>
+              <p>Vous pourrez retrouver sur ce site toutes les informations me concerant, mon expérience, ma formation et plein
+                d&apos;autres ! N&apos;hésitez pas à me contacter !
+              </p>
+            </div>
+            <div className="col ms-5">
+              <Image src={Id} alt="" height='250'></Image>
+            </div>
+          </div>
+          <div className="mt-5">
+            <p>
+              Né le 5 décembre 2001 à Cherbourg-en-Cotentin dans la Manche en Normandie, j&apos;ai grandi
+              et vécu dans cette ville jusqu&apos;à mes 19 ans. Je suis ensuite parti étudier dans la ville de
+              Poitiers, près du Futuroscope pour mon diplôme d&apos;ingénieur. Sportif et voyageur, je profite
+              des expériences qu&apos;offre la vie pour bouger et faire de nouvelles rencontres. Le sport m&apos;apporte
+              des qualités de travail d&apos;équipe et de détermination mais aussi de leading grâce à une longue
+              expérience de capitanat. Le voyage m&apos;apporte de son côté des qualités d&apos;expérience
+              internationale et d&apos;adaptation à des environnements nouveaux. 
+            </p>
+          </div>
+
+        </>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
+import Id from "../public/images/Id.png"
+import Image from "next/image"
+import About from "./about"
+export default function App() {
   return (
-    <div>
-      <h1>Hey !</h1>
-    </div>
+    <>
+      <div className="min-vh-100 row g-0">
+        <div className="col-2 text-bg-dark pt-3">
+          <Image className="ms-3" src={Id} alt="" height="150"></Image>
+          <h4 className="mt-3 ms-3">LANGLOIS Noah</h4>
+          <p className="ms-4 ps-3">Étudiant ingénieur</p>
+          <ul className="nav flex-column mt-5">
+            <div className="btn-group-vertical" role="group">
+              <button type="button" className="btn btn-secondary w-100 active p-3">À propos</button>
+              <button type="button" className="btn btn-secondary w-100 p-3">Formation</button>
+              <button type="button" className="btn btn-secondary w-100 p-3">Expérience</button>
+              <button type="button" className="btn btn-secondary w-100 p-3">Projets</button>
+              <button type="button" className="btn btn-secondary w-100 p-3">Mais encore...</button>
+              <button type="button" className="btn btn-secondary w-100 p-3">Contact</button>
+            </div> 
+          </ul>
+        </div>
+        <div className="col p-5 overflow-auto">
+          <About></About>
+        </div>
+      </div>
+    </>
   )
 }
