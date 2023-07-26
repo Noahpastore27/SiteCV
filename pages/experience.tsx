5d22b4a9cd6e688d7ba95fc7a8ce5bb89301e87c
noah.langlois <noahlanglois@hotmail.com>
2023-07-26 13:41:41 +0000

    Job etudiant done

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -1,8 +1,119 @@
 import Image from "next/image";
 import emn from "../public/images/emn.svg"
 import leclerc from "../public/images/leclerc.png"
-import ensma from "../public/images/ensma.png"
+import endel from "../public/images/endel.png"
+import { useState } from "react";
 export default function Experience () {
+
+    const [jobetu,setJobetu]=useState(0)
+
+    const DefJob = () => {
+        if (jobetu===0) {
+            setJobetu(1)
+        }
+        if (jobetu===1) {
+            setJobetu(0)
+        }
+    }
+
+    function MoreJobetu () {
+        if (jobetu===0) {
+            return (<></>);
+        }
+        if (jobetu===1) {
+            return (
+                <div className="fade_in_top">
+                    <div className="row g-0 mt-3">
+                        <div className="col-4 card">
+                            <div className="card-body">
+                                <div className="height_card">
+                                    <h2>Agent d&apos;entretien, <br></br> Gardiennage</h2>
+                                    <p>Nettoyage de résidences, poubelles, halls.</p>
+                                    <br></br>
+                                    <a href="http://societe-emn.fr/" target="_blank" rel="noopener noreferrer">
+                                    EMN</a>
+                                    <p>Cherbourg-en-Cotentin</p>
+                                    <div className="text-center mt-5">
+                                        <Image src={emn} alt="" height="50"></Image>
+                                    </div>
+                                    <div className="text-center mt-5">
+                                        <h3 className="">2019</h3>
+                                    </div>
+                                </div>
+                            </div> 
+                        </div>
+                        <div className="col-4">
+                            <div className="card">
+                                <div className="card-body">
+                                    <div className="height_card">
+                                        <div className="">
+                                            <h2>Livreur,<br></br> Vendeur</h2>
+                                            <p>Livraison de matériel électroménager et vente dans l&apos;espace multimédia</p>
+                                            <br></br>
+                                            <a href="https://www.e.leclerc/mag/e-leclerc-tourlaville" target="_blank" rel="noopener noreferrer">
+                                            E.Leclerc</a>
+                                            <p>Tourlaville</p>
+                                        </div>
+                                        <div className="mt-5 text-center">
+                                            <Image src={leclerc} alt="" height="50"></Image>
+                                        </div>
+                                        <div className="text-center mt-5">
+                                            <h3>Été 2020-2021</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-4">
+                            <div className="card">
+                                <div className="card-body">
+                                    <div className="height_card">
+                                        <div className="">
+                                            <h2>Logisticien, <br></br>Gestion de stock</h2>
+                                            <p>Gestion de stock de matériel d&apos;usinage, création d&apos;une macro 
+                                            excel VBA de gestion de stock</p>
+                                            <a href="https://www.altradendel.com/en/" target="_blank" rel="noopener noreferrer">
+                                            ENDEL</a>
+                                            <p>Tourlaville</p>
+                                        </div>
+                                        <div className="text-center mt-5">
+                                            <Image src={endel} alt="" height="50"></Image>
+                                        </div>
+                                        <div className="text-center mt-5">
+                                            <h3>Août 2022</h3>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+    }
+
+
+    function MoreBtn () {
+        if (jobetu===0) {
+            return (
+                <button className="btn btn-secondary" onClick={() => DefJob()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                        <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                    </svg>
+                </button>
+            );
+        }
+        if (jobetu===1) {
+            return (
+                <button className="btn btn-secondary" onClick={() => DefJob()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                        <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                    </svg>
+                </button>
+            );
+        }
+    }
+
     return (
         <div className="vh-100">
             <div className="Animation_text_home">
@@ -21,66 +132,15 @@ export default function Experience () {
             <div className="scale_in_center">
                 <div className="card">
                     <div className="card-body">
-                        <h2>Jobs étudiants</h2>
                         <div className="row g-0">
-                            <div className="col-4 card mt-5 ">
-                                <div className="card-body">
-                                    <h2>Agent d&apos;entretien, <br></br>Nettoyage des résidences, <br></br> Gardiennage</h2>
-                                    <a href="http://societe-emn.fr/" target="_blank" rel="noopener noreferrer">
-                                    EMN</a>
-                                    <p>Cherbourg-en-Cotentin</p>
-                                    <p>(Job étudiant)</p>
-                                </div>
-                                <div className="mt-4 pt-2">
-                                    <Image src={emn} alt="" height="40"></Image>
-                                </div>
-                                <div className="ms-5 mt-4 pt-2">
-                                    <h3>2019</h3>
-                                </div>
-                            </div>
                             <div className="col-4">
-                                <div className="card mt-5">
-                                    <div className="card-body">
-                                        <div className="">
-                                            <div className="pe-5">
-                                                <h2>Livreur, Vendeur</h2>
-                                                <p>Livraison de matériel électroménager et vente dans l&apos;espace multimédia</p>
-                                                <a href="https://www.e.leclerc/mag/e-leclerc-tourlaville" target="_blank" rel="noopener noreferrer">
-                                                E.Leclerc</a>
-                                                <p>Tourlaville</p>
-                                            </div>
-                                            <div className="mt-4 ps-5 pt-3">
-                                                <Image src={leclerc} alt="" height="70"></Image>
-                                            </div>
-                                            <div className="mt-5 ps-5 pt-3">
-                                                <h3>Été 2020-2021</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <MoreBtn />
                             </div>
-                            <div className="col-4">
-                                <div className="card mt-5">
-                                    <div className="card-body">
-                                        <div className="">
-                                            <div className="">
-                                                <h2>Ingénieur ISAE-ENSMA</h2>
-                                                <p>&#40;Spécialisation informatique, avionique et systèmes&#41;</p>
-                                                <a href="https://www.ensma.fr/" target="_blank" rel="noopener noreferrer">
-                                                ISAE-ENSMA</a>
-                                                <p>Poitiers - Fututoscope</p>
-                                            </div>
-                                            <div className="ps-5 mt-3">
-                                                <Image src={ensma} alt="" height="100"></Image>
-                                            </div>
-                                            <div className="mt-5">
-                                                <h3>2021-2024</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                            <div className="col-4 text-center">
+                                <h2>Jobs étudiants</h2>
                             </div>
                         </div>
+                        <MoreJobetu />
                     </div>
                 </div>
 
